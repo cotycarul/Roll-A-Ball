6f8b19b7beaec5d4df40cbee1600655d4e1cd4c3
Costin <you@example.com>
2016-06-21 19:13:00 +0300

    s

---
+++ RollABallTutorial/Assets/Scripts/CameraController.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+var player :GameObject;
+var offset :Vector3;
+function Start () {
+    offset = transform.position - player.transform.position;
+}
+
+function LateUpdate () {
+    transform.position = player.transform.position + offset;
+}
---
+++ RollABallTutorial/Assets/Scripts/PlayerController.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var rb :Rigidbody;
+var speed : float = 100;
+var count : int;
+var countText: UnityEngine.UI.Text;
+
+
+
+function Start () {
+    rb = GetComponent.<Rigidbody>();
+    count = 0;
+    countText.text = "Count: ";
+}
+
+function FixedUpdate () {
+    var moveHorizontal : float =  Input.GetAxis("Horizontal");
+    var moveVertical : float =  Input.GetAxis("Vertical");
+    var vector3 = new Vector3(moveHorizontal,0.0f,moveVertical);
+    rb.AddForce (vector3 * speed);
+}
+function OnTriggerEnter (other : Collider) {
+    if(other.gameObject.CompareTag("Pick Up"))
+    {
+        other.gameObject.SetActive(false);
+        count++;
+    }
+}
